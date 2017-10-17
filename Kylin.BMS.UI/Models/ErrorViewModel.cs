ab6856793dd1736865ffc9e36625696bee9ac32a
threecolors <threecolors@126.com>
2017-10-17 16:16:15 +0800

    添加项目文件。

---
+++ Kylin.BMS.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Kylin.BMS.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
